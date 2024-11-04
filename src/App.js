3b72f25e8dfc5326bd98d397d2cf2df75f1d0a80
mudasarasim <117173890+mudasarasim@users.noreply.github.com>
2024-11-04 06:56:34 +0500

    kl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
