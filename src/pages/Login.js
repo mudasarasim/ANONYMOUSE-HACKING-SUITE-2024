16893174350f6aaf678e80d82beeaf14e6d71467
mudasarasim <117173890+mudasarasim@users.noreply.github.com>
2024-11-03 17:30:35 -0800

    jkuu

---
+++ src/pages/Login.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Login.css';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    // Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Check credentials
+        if (email === 'admin@gmail.com' && password === 'admin') {
+            localStorage.setItem('role', 'admin'); // Set role as student in localStorage
+            navigate('/dashboard');
+        } else {
+            toast.error('Wrong credentials, please try again!', {
+                position: 'top-center',
+                autoClose: 3000,
+                hideProgressBar: true,
+            });
+        }
+    };
+
+    return (
+        <div className='main'>
+
+            <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+
+                <div class="signin">
+
+                    <div class="content">
+
+                        <img src='logo.png' alt='logo' style={{height: '120px'}} />
+                        <h2 style={{marginTop: '-28px', marginBottom: '-17px'}}>Sign in </h2>
+
+                        <form onSubmit={handleSubmit} class="form">
+
+                            <div class="inputBox">
+
+                                <input
+                                    type="email"
+                                    id="login"
+                                    className="fadeIn second"
+                                    name="login"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required /> <i>Email</i>
+
+                            </div>
+
+                            <div class="inputBox">
+
+                                <input
+                                    type="password"
+                                    id="password"
+                                    className="fadeIn third"
+                                    name="login"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+
+                                    required /> <i>Password</i>
+
+                            </div>
+
+                            <div class="links"> <a href="/">Forgot Password</a> 
+
+                            </div>
+
+                            <div class="inputBox">
+
+                                <input type="submit" value="Login" />
+
+                            </div>
+
+                        </form>
+
+                    </div>
+
+                </div>
+            </section>
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Login;
