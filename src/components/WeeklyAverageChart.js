b39aae60910e23dc0aa1774318f4087aeac82740
mudasarasim <117173890+mudasarasim@users.noreply.github.com>
2024-11-07 18:11:14 +0500

    34rty

--- src/components/WeeklyAverageChart.js
+++ src/components/WeeklyAverageChart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
-import { color } from 'chart.js/helpers';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
